95746f815789ebd5734934bd0ccbec8f1d989cdb
toshiakisan1127 <toshiakisan1127@gmail.com>
2023-07-22 08:09:55 +0900

    header, metaタグの修正

--- nuxt-playground-toshiaki/nuxt.config.ts
+++ nuxt-playground-toshiaki/nuxt.config.ts
@@ -2,4 +2,16 @@
 export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "nuxt playground toshiaki",
+      htmlAttrs: {
+        lang: "ja",
+      },
+      meta: [
+        { charset: "UTF-16" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+  },
 });
